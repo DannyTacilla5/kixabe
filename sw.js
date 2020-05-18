e4649edade2ac4c62e60489ee322957d6b681850
DannyTacilla5 <dtacillac12@unc.edu.pe>
2020-05-18 11:30:53 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('airhorner').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/index.html?homescreen=1',
+        '/?homescreen=1',
+        '/styles.css'
+      ]);
+    })
+  );
+ });
